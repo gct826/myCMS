2435d90c5f2e624bc93faa81f9c2afc60222ced6
GCT826 <gct826@gmail.com>
2013-07-22 22:59:07 -0500

    Adding in HomeController Index()

---
+++ myCMS.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using myCMS;
+using myCMS.Controllers;
+
+namespace myCMS.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            var controller = new HomeController(); 
+
+            // Act
+            var result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
+
+
+
+
+
